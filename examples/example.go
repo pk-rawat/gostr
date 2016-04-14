1ff307c98633ec2c1e7a080863a8e1d2eb04d22e
Jörg Thalheim <joerg@higgsboson.tk>
2016-04-14 20:05:55 +0200

    fix example
    map was not initialized

--- examples/example.go
+++ examples/example.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/pk-rawat/gostr/src"
 )
 
 func main() {
-	var values map[string]interface{}
+	values := make(map[string]interface{})
 	values["a"] = 5
-	result := gostr.Evaluate("a * 2", values)
+	values["b"] = -1
+	values["c"] = false
+	if len(os.Args) < 2 {
+		fmt.Printf("USAGE: %s EXPR", os.Args[0])
+		os.Exit(1)
+	}
+	result := gostr.Evaluate(os.Args[1], values)
 	fmt.Println("Result:", result)
 }
